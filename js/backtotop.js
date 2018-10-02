139eca770070580cd528df1984c5e63067a1357c
jb3rns <jb3rns@gmail.com>
2018-10-02 09:29:49 -0500

    Fixed the jerky scroll down from initial scroll at window 0

--- js/backtotop.js
+++ js/backtotop.js
@@ -1,5 +1,4 @@
 (function ($, Drupal) {
-
     Drupal.behaviors.STARTER = {
       attach: function(context, settings) {
         // Get your Yeti started.
@@ -9,14 +8,12 @@
          // This function runs every time the user scrolls the page.
          $(window).scroll(function(){
            // Check weather the user has scrolled down (if "scrollTop()"" is more than 0)
-           if($(window).scrollTop() > 0){ 
+           if($(window).scrollTop() >= 50){ 
                // If it's more than or equal to 0, show the toTop button.
-               console.log("is more");
                $("#toTop").fadeIn("slow");
            }
            else {
                // If it's less than 0 (at the top), hide the toTop button.
-               console.log("is less");
                $("#toTop").fadeOut("slow");
            }
           })   
@@ -34,5 +31,4 @@
         
       }
     };
-  
-  })(jQuery, Drupal);
+})(jQuery, Drupal);

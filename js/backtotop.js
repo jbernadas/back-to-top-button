a0b8f1a4925a97355dbcd4a017832297cf478d62
jb3rns <jb3rns@gmail.com>
2018-10-04 10:34:27 -0500

    Added page.stop() to stop animation anytime user scrolls again

--- js/backtotop.js
+++ js/backtotop.js
@@ -8,25 +8,26 @@
          // This function runs every time the user scrolls the page.
          $(window).scroll(function(){
            // Check whether the user has scrolled down (if "scrollTop()"" is more than 0)
-           if($(window).scrollTop() >= 50){ 
-               // If it's more than or equal to 50, show the toTop button.
+           if($(window).scrollTop() >= 40){ 
+               // If it's more than or equal to 0, show the toTop button.
                $("#toTop").fadeIn("slow");
            }
            else {
-               // If it's less than 0 (at the top), hide the toTop button.
+               // If it's less than 1 (at the top), hide the toTop button.
                $("#toTop").fadeOut("slow");
            }
           })   
         });    
         // When the user clicks the toTop button, we want the page to scroll to the top slowly.
-        $("#toTop").click(function(e){
+        $("#toTop").click(function(event){
            // Disable the default behaviour when a user clicks an empty anchor link.
            // (The page jumps to the top instead of animating)
-           e.preventDefault(); 
+           event.preventDefault(); 
            // Animate the scrolling motion.
            $("html, body").animate({
              scrollTop:0
              },"slow");
+            page.stop();
         });  
       }
     };
